bdbae8cf7d5b96be0f7b565d3ebbcb943b51ac97
Ethan Flow <109869245+MrF1ow@users.noreply.github.com>
2023-11-03 23:42:46 -0700

    Update file_manager.py
    Fixed JPG issue

--- file_manager.py
+++ file_manager.py
@@ -1,5 +1,6 @@
 import os
-from tkinter import filedialog
+from tkinter import Tk
+from tkinter import filedialog  # pip install tk
 
 
 class FileManager:
@@ -7,14 +8,22 @@ class FileManager:
         self.file = None
 
     def get_file(self):
+        valid_file_types = [
+            ("Image Files", "*.png *.jpeg *.jpg *.gif *.bmp *.tiff"),
+            ("PNG files", "*.png"),
+            ("JPEG files", "*.jpeg *.jpg"),
+            ("GIF files", "*.gif"),
+            ("BMP files", "*.bmp"),
+            ("TIFF files", "*.tiff")
+        ]
 
-        valid_file_types = [("Image files", "*.png *.jpeg *.gif *.bmp *.tiff")]
-
-        file_path = filedialog.askopenfilename(filetypes=valid_file_types)  # Prompt user to select image
+        file_path = filedialog.askopenfilename(
+            filetypes=valid_file_types)  # Prompt user to select image
 
         if file_path:
             self.file = file_path  # Update file attribute with path of file selected
 
+    # this should be an option
     def save_file(self, content):
         if self.file:
             content.save(self.file)  # Overwrites existing file with new edits
@@ -24,14 +33,15 @@ class FileManager:
 
         valid_file_types = [
             ("PNG files", "*.png"),
-            ("JPEG files", " *.jpeg"),
+            ("JPEG files", "*.jpg"),
             ("GIF files", "*.gif"),
             ("BMP files", "*.bmp"),
             ("TIFF files", "*.tiff")
         ]
 
         # Prompts user to save the file under the desired name and file format
-        file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=valid_file_types)
+        file_path = filedialog.asksaveasfilename(
+            defaultextension=".png", filetypes=valid_file_types)
 
         if file_path:
             content.save(file_path)  # Saves the file in that destination
@@ -43,6 +53,9 @@ class FileManager:
                 self.file = None  # Clear the file attribute
             except OSError:
                 print(f"Error: {OSError}")
+
+
+
 
 
 
