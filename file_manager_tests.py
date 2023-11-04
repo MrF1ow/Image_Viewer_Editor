b2cd0e64ed4b9b81d851a202d8cd44c2c0e00d44
Ethan Flow <109869245+MrF1ow@users.noreply.github.com>
2023-11-03 23:43:40 -0700

    Update file_manager_tests.py
    Added image opening tests, and made looping stop

--- file_manager_tests.py
+++ file_manager_tests.py
@@ -1,5 +1,6 @@
-from file_manager import FileManager
+from file_manager import FileManager  # pip install file_manager
 from PIL import Image
+import sys
 
 # Example of functions
 fm = FileManager()
@@ -9,9 +10,8 @@ fm.get_file()  # Retrieves file
 if fm.file:
     print("Selected file: ", fm.file)
     image = Image.open(fm.file)  # Opens the image
-
-    fm.save_as_file(image)  # Make a copy
-    print("Success")
+    image.show()
+    # fm.save_as_file(image)  # Make a copy
 else:
     print("Error")
 
@@ -26,13 +26,8 @@ if fm.file:
 else:
     print("Error")
 
-if fm.file:
-    print("Selected file: ", fm.file)
-    image = Image.open(fm.file)  # Opens the image
-
-    fm.save_as_file(image)  # Make a copy with the edit
-    print("Success")
-else:
-    print("Error")
-
+# this should be an option
 fm.delete_file()  # Delete the original file
+
+sys.exit(0) # used to stop infinite looping
+
