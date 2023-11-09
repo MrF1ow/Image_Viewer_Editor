6f2cbc6d705340af602acfd814938e77675b2b03
Brayden Edwards <edwabray@oregonstate.edu>
2023-11-09 13:34:53 -0800

    AdvancedEditorTools and ImageProperties class created

---
+++ advanced_editor_tools.py
@@ -0,0 +1,10 @@
+
+
+class EditorTools:
+    def __init__(self) -> None:
+        pass
+    
+    def set_brightness(desired_saturation: int) -> None:
+        if 0 <= desired_saturation <= 100:
+            pass
+            
