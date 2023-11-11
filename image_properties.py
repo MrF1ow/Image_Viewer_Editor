bf497afb73eb193b2e38fae2b605b59a6816293b
Ethan Flow <eflow1280@gmail.com>
2023-11-10 20:56:13 -0800

    Update image_properties.py

--- image_properties.py
+++ image_properties.py
@@ -5,6 +5,10 @@ class ImageProperties:
     is_flipped_horz: bool=False
     is_flipped_vert: bool=False
 
-    rotation: int=0
+    image_width: int=0
+    image_height: int=0
 
-    brightness: int=0
+    rotation: int=0
+    brightness: int=1
+
+    blur_size: int=0
