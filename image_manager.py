fab9166f285b297898550ca12216584e61ebc8a1
Ethan Flow <eflow1280@gmail.com>
2023-11-10 20:55:58 -0800

    8k images now work

--- image_manager.py
+++ image_manager.py
@@ -1,19 +1,21 @@
 from tkinter import Frame, Canvas, CENTER
 from PIL import Image, ImageTk
 import cv2
+import math
+from image_properties import ImageProperties
 
 
 class ImageManager(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master=master, bg="grey",
-                       width=960, height=540)
+                       width=1440, height=810)
 
         self.current_image = None  # store currently displayed image
 
         # inside of the frame, make a canvas for image using the 'Canvas' widget (look it up)
-        self.canvas = Canvas(self, bg="black", width=960, height=540)
+        self.canvas = Canvas(self, bg="black",  width=1440, height=810)
         # center the image
-        self.canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.canvas.place(relx=0.5, rely=0.5, anchor="center")
 
     def display_image(self, img=None):
         if img is None:
@@ -32,22 +34,21 @@ class ImageManager(Frame):
         new_width = width
 
         # need to check if the image size is bigger than the frame
-        # not gonna lie, chatGPT wrote this part, but it works
         if height > self.winfo_height() or width > self.winfo_width():
             if ratio < 1:
                 new_width = self.winfo_width()
-                new_height = int(new_height * ratio)
+                new_ratio = new_width / width
+                new_height = int(new_height * new_ratio)
             else:
                 new_height = self.winfo_height()
-                new_width = int(new_height * (width / height))
+                new_width = int(math.floor(new_height * (width / height)))
 
+        self.current_image = cv2.resize(image, (new_width, new_height))
+        self.current_image = ImageTk.PhotoImage(Image.fromarray(self.current_image))
 
-        # need to figure out for portraits
-        if new_width > 0 and new_height > 0:
-            # resizes image to fit
-            self.current_image = cv2.resize(image, (new_width, new_height))
-            self.current_image = ImageTk.PhotoImage(Image.fromarray(self.current_image))
+        self.canvas.config(width=new_width, height=new_height)
+        self.canvas.create_image(new_width / 2, new_height / 2, anchor="center", image=self.current_image)
 
-            self.canvas.config(width=new_width, height=new_height)
-            self.canvas.create_image(new_width / 2, new_height / 2, anchor=CENTER, image=self.current_image)
+        ImageProperties.image_height = new_height
+        ImageProperties.image_width = new_width
 
